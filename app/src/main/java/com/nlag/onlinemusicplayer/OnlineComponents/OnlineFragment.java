e800a611b9167c44d1636d50c168f1ed6c73756c
NLag <NLag@users.noreply.github.com>
2017-11-21 04:26:40 +0700

    Add adapter, item class, item layout for music ranking listview.

--- app/src/main/java/com/nlag/onlinemusicplayer/OnlineComponents/OnlineFragment.java
+++ app/src/main/java/com/nlag/onlinemusicplayer/OnlineComponents/OnlineFragment.java
@@ -1,12 +1,18 @@
 package com.nlag.onlinemusicplayer.OnlineComponents;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -19,6 +25,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nlag on 11/20/17.
@@ -31,7 +38,7 @@ public class OnlineFragment extends Fragment {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         View onlineFragmentView = inflater.inflate(R.layout.online_fragment,container,false);
 
-        //get Music Ranking
+        //get Music Ranking json
         ArrayList<JSONObject> rank10songs = getRankFromZingMP3();
 
         return onlineFragmentView;
@@ -67,4 +74,61 @@ public class OnlineFragment extends Fragment {
         ((MainAppQueue) getActivity().getApplication()).getQueue().add(request);
         return jsonSongsList;
     }
+
+    public class MusicRankingAdapter extends BaseAdapter {
+
+        private List<OnlineMusicRankingSong> rankingsongList;
+        private LayoutInflater layoutInflater;
+        private Context context;
+
+        public MusicRankingAdapter(Context context, List<OnlineMusicRankingSong> rankingsongList) {
+            this.context = context;
+            this.layoutInflater = LayoutInflater.from(context);
+            this.rankingsongList = rankingsongList;
+        }
+
+        @Override
+        public int getCount() {
+            return rankingsongList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return rankingsongList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.online_musicranking_list_item, null);
+                holder = new ViewHolder();
+                holder.thumbnail = (ImageView) convertView.findViewById(R.id.musicrank_item_thumb);
+                holder.name = (TextView) convertView.findViewById(R.id.musicrank_item_name);
+                holder.artist = (TextView) convertView.findViewById(R.id.musicrank_item_artist);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            OnlineMusicRankingSong song = this.rankingsongList.get(position);
+            holder.name.setText(song.getName());
+            holder.artist.setText(song.getArtist());
+            holder.thumbnail.setImageBitmap(song.getThumb());
+
+            return convertView;
+        }
+        class ViewHolder {
+            ImageView thumbnail;
+            TextView name;
+            TextView artist;
+        }
+    }
+
+
 }
