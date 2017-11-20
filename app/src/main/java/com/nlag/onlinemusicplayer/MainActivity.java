1fad48784922f4129daaa821b8d2fb8f516e8481
NLag <nlag@users.noreply.github.com>
2017-11-20 22:09:17 +0700

    Create custom toolbar, add values files to git.

--- app/src/main/java/com/nlag/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/com/nlag/onlinemusicplayer/MainActivity.java
@@ -8,6 +8,10 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.nlag.onlinemusicplayer.LocalComponents.LocalFragment;
 import com.nlag.onlinemusicplayer.OnlineComponents.OnlineFragment;
@@ -27,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         TabLayout mainTabLayout = (TabLayout) findViewById(R.id.maintab);
         mainTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         mainTabLayout.setupWithViewPager(mainPager);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
 
     }
 
@@ -59,6 +66,40 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int page) {
             // returns a tab title corresponding to the specified page
             return titles[page];
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_toolbar_actions, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refesh:
+
+                return true;
+            case R.id.action_search:
+                // User chose the "Search" item, show the app search UI...
+                return true;
+
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
     }
 
