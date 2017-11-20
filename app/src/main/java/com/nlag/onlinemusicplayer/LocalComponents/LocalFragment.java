6681eeea860aaf7d6bc5c9daf226e4938f2c17ab
NLag <nlag@users.noreply.github.com>
2017-11-20 20:25:23 +0700

    Add local and online fragment in viewpager to  Main activity

--- app/src/main/java/com/nlag/onlinemusicplayer/LocalComponents/LocalFragment.java
+++ app/src/main/java/com/nlag/onlinemusicplayer/LocalComponents/LocalFragment.java
@@ -18,6 +18,6 @@ public class LocalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.online_fragment,container,false);
+        return inflater.inflate(R.layout.local_fragment,container,false);
     }
 }
--- app/src/main/java/com/nlag/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/com/nlag/onlinemusicplayer/MainActivity.java
@@ -1,7 +1,15 @@
 package com.nlag.onlinemusicplayer;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nlag.onlinemusicplayer.LocalComponents.LocalFragment;
+import com.nlag.onlinemusicplayer.OnlineComponents.OnlineFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PagerAdapter mainAdapter = new MainFragmentPagerAdapter( getSupportFragmentManager());
+        ViewPager mainPager = (ViewPager) findViewById(R.id.mainpager);
+        mainPager.setOffscreenPageLimit(2);
+        mainPager.setAdapter(mainAdapter);
+
     }
+
+    public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private final int PAGE_COUNT = 2;
+        private String titles[] = new String[] { "Online", "Local" };
+
+        public MainFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        // number of pages for a ViewPager
+        public Fragment getItem(int page) {
+            // returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0: return new OnlineFragment();
+                case 1: return new LocalFragment();
+            }
+            return null; // failsafe
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+            // returns a tab title corresponding to the specified page
+            return titles[page];
+        }
+    }
+
+
 }
